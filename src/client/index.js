467a4a8233addad35214f463e1b3d07d2c464d89
arijoon <ari@ianaied.com>
2018-12-29 23:09:09 -0500

    Bigbang 2

--- src/client/index.js
+++ src/client/index.js
@@ -1,3 +1,4 @@
 import './css/style.css';
 import './scss/main.scss';
+import './scss/b4/bootstrap.scss';
 console.log('DadamVaaay');
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require("webpack-md5-hash");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
   entry: { main: './src/client/index.js' },
@@ -12,29 +13,31 @@ module.exports = {
     path: path.resolve(__dirname, 'dist/public'),
     filename: '[name].[chunkhash].js'
   },
-  target: 'node', // update from 23.12.2018
-  externals: [nodeExternals()], // update from 23.12.2018
+  target: 'node',
+  externals: [nodeExternals()],
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader"
+          }
+        }, 
+        {
+          test: /\.(sa|sc|c)ss$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'postcss-loader',
+            'sass-loader',
+          ],
         }
-      },
-      {
-        test: /\.css$/,
-        use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader']
-      },
-      {
-        test: /\.scss$/,
-        use: [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader']
-      }
     ]
   },
   plugins: [ 
-    new CleanWebpackPlugin('dist/public', {} ),
+    new UglifyJSPlugin({extractComments: 'all'}),
+    new CleanWebpackPlugin('dist/public', {} ), 
     new MiniCssExtractPlugin({
       filename: "style.[contenthash].css"
     }),
